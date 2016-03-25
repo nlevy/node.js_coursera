7d2d7feb34a0a45523561219b8885f553e2d3c2c
nlevy <nir1.levy1@gmail.com>
2016-03-25 23:34:12 +0300

    assignment2

---
+++ src/assignment2/models/promotions.js
@@ -0,0 +1,34 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+require('mongoose-currency').loadType(mongoose);
+var Currency = mongoose.Types.Currency;
+
+var promotionSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    label: {
+        type: String,
+        default: ""
+    },
+    price: {
+        type: Currency,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+var Promotions = mongoose.model('Promotion', promotionSchema);
+
+module.exports = Promotions;
